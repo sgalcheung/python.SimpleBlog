204d63b9a0b2f757896423ba241813b61601ecf4
sgal <zhangsigao@live.com>
2019-09-06 09:03:16 +0800

    only point operators can be used

--- flaskr/auth/views.py
+++ flaskr/auth/views.py
@@ -91,7 +91,7 @@ def login():
     if error is None:
       # store the user id in a new session and return to the index
       session.clear()
-      session['user_id'] = user['id']
+      session['user_id'] = user.id
       return redirect(url_for('manage.index'))  # jump to management home page
 
     flash(error)
